ae7d9e9370759d8515e74556c4119e4f7b991a3a
Dmitriy <xlxdimsanxlx@gmail.com>
2024-04-29 18:30:47 +0300

    feat: adding models to the admin area

--- completed_work/admin.py
+++ completed_work/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from completed_work.models import CompletedWork, FotoWork, CommentsWork
+
+
+@admin.register(CompletedWork)
+class CompletedWorkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'overall_plan', 'created_at', 'updated_at']
+
+
+@admin.register(FotoWork)
+class FotoWorkAdmin(admin.ModelAdmin):
+    list_display = ['file', 'completed_work', 'created_at', 'updated_at']
+
+
+@admin.register(CommentsWork)
+class CommentsWorkAdmin(admin.ModelAdmin):
+    list_display = ['user', 'comment', 'completed_work', 'created_at', 'updated_at']
